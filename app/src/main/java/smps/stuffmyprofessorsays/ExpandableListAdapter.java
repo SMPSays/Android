7210a14a1b41b2831a3aa650e60e1c08fd8a04eb
Tricia Nemiroff <tricianemiroff@Tricias-Mac.local>
2016-01-30 18:29:19 -0800

    initial commit

---
+++ app/src/main/java/smps/stuffmyprofessorsays/ExpandableListAdapter.java
@@ -0,0 +1,199 @@
+package smps.stuffmyprofessorsays;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by tricianemiroff on 1/26/16.
+ */
+public class ExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private List<String> listHeader;
+    private HashMap<String, List<String>> listChild;
+
+
+    public ExpandableListAdapter(Context context1, List<String> listHeader1, HashMap<String, List<String>> listChild1){
+        this.context = context1;
+        this.listHeader = listHeader1;
+        this.listChild = listChild1;
+    }
+
+    /**
+     * Gets the number of groups.
+     *
+     * @return the number of groups
+     */
+    @Override
+    public int getGroupCount() {
+        return this.listHeader.size();
+    }
+
+    /**
+     * Gets the number of children in a specified group.
+     *
+     * @param groupPosition the position of the group for which the children
+     *                      count should be returned
+     * @return the children count in the specified group
+     */
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return this.listChild.get(this.listHeader.get(groupPosition)).size();
+    }
+
+    /**
+     * Gets the data associated with the given group.
+     *
+     * @param groupPosition the position of the group
+     * @return the data child for the specified group
+     */
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.listHeader.get(groupPosition);
+    }
+
+    /**
+     * Gets the data associated with the given child within the given group.
+     *
+     * @param groupPosition the position of the group that the child resides in
+     * @param childPosition the position of the child with respect to other
+     *                      children in the group
+     * @return the data of the child
+     */
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return this.listChild.get(this.listHeader.get(groupPosition)).get(childPosition);
+    }
+
+    /**
+     * Gets the ID for the group at the given position. This group ID must be
+     * unique across groups. The combined ID (see
+     * {@link #getCombinedGroupId(long)}) must be unique across ALL items
+     * (groups and all children).
+     *
+     * @param groupPosition the position of the group for which the ID is wanted
+     * @return the ID associated with the group
+     */
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    /**
+     * Gets the ID for the given child within the given group. This ID must be
+     * unique across all children within the group. The combined ID (see
+     * {@link #getCombinedChildId(long, long)}) must be unique across ALL items
+     * (groups and all children).
+     *
+     * @param groupPosition the position of the group that contains the child
+     * @param childPosition the position of the child within the group for which
+     *                      the ID is wanted
+     * @return the ID associated with the child
+     */
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    /**
+     * Indicates whether the child and group IDs are stable across changes to the
+     * underlying data.
+     *
+     * @return whether or not the same ID always refers to the same object
+     * @see
+     */
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    /**
+     * Gets a View that displays the given group. This View is only for the
+     * group--the Views for the group's children will be fetched using
+     * {@link #getChildView(int, int, boolean, View, ViewGroup)}.
+     *
+     * @param groupPosition the position of the group for which the View is
+     *                      returned
+     * @param isExpanded    whether the group is expanded or collapsed
+     * @param convertView   the old view to reuse, if possible. You should check
+     *                      that this view is non-null and of an appropriate type before
+     *                      using. If it is not possible to convert this view to display
+     *                      the correct data, this method can create a new view. It is not
+     *                      guaranteed that the convertView will have been previously
+     *                      created by
+     *                      {@link #getGroupView(int, boolean, View, ViewGroup)}.
+     * @param parent        the parent that this view will eventually be attached to
+     * @return the View corresponding to the group at the specified position
+     */
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String headerTitle = (String) getGroup(groupPosition);
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_header, null);
+        }
+
+        TextView lblListHeader = (TextView) convertView
+                .findViewById(R.id.lblListHeader);
+        lblListHeader.setTypeface(null, Typeface.BOLD);
+        lblListHeader.setText(headerTitle);
+
+        return convertView;
+    }
+
+    /**
+     * Gets a View that displays the data for the given child within the given
+     * group.
+     *
+     * @param groupPosition the position of the group that contains the child
+     * @param childPosition the position of the child (for which the View is
+     *                      returned) within the group
+     * @param isLastChild   Whether the child is the last child within the group
+     * @param convertView   the old view to reuse, if possible. You should check
+     *                      that this view is non-null and of an appropriate type before
+     *                      using. If it is not possible to convert this view to display
+     *                      the correct data, this method can create a new view. It is not
+     *                      guaranteed that the convertView will have been previously
+     *                      created by
+     *                      {@link #getChildView(int, int, boolean, View, ViewGroup)}.
+     * @param parent        the parent that this view will eventually be attached to
+     * @return the View corresponding to the child at the specified position
+     */
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+        final String childText = (String) getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater infalInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = infalInflater.inflate(R.layout.list_item, null);
+        }
+
+        TextView txtListChild = (TextView) convertView
+                .findViewById(R.id.lblListItem);
+
+        txtListChild.setText(childText);
+        return convertView;
+    }
+
+    /**
+     * Whether the child at the specified position is selectable.
+     *
+     * @param groupPosition the position of the group that contains the child
+     * @param childPosition the position of the child within the group
+     * @return whether the child is selectable.
+     */
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return true;
+    }
+}
---
+++ app/src/main/java/smps/stuffmyprofessorsays/MainActivity.java
@@ -0,0 +1,22 @@
+package smps.stuffmyprofessorsays;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_page);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
